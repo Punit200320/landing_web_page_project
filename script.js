08d17ec5a86f88dc75ccb729907813f3e986079e
Punit200320 <147536292+Punit200320@users.noreply.github.com>
2024-06-09 11:48:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav ul li a');
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetSection = document.querySelector(this.getAttribute('href'));
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+=======
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav ul li a');
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetSection = document.querySelector(this.getAttribute('href'));
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+>>>>>>> 968c1b75c45b62613d749ffafacb4305ffca6c95
